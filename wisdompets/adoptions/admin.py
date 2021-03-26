bfe91d1e16a7a1ec20ef5b841ad7bd77dde1ef43
Louis Hector <appdev@hectorcode.com>
2021-03-26 11:02:16 +0100

    Chapter 3 completed.

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Pet
 
+
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
+	# Use to provide a more adapted view of the rows and colums
     list_display = ['name', 'species', 'breed', 'age', 'sex']
